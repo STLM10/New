d4c033accc51d8b2e2a62f49e7aadf539ff5c388
Vikram Anand <101736189+STLM10@users.noreply.github.com>
2023-01-11 02:49:45 +0530

    Update new.py

--- new.py
+++ new.py
@@ -19,8 +19,8 @@ if st.button('check'):
     img = np.array(img)
     img = img / 255.0
     img = img.resize((50,50))
-    img1 = img.flatten()
-    a=model.predict(img1)
+    img.flatten()
+    a=model.predict(img)
     if a<0.5:
         st.text('cat')
     else:
