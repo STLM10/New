c05e5501338b077cfed5a9d60189de8a140074fe
Vikram Anand <101736189+STLM10@users.noreply.github.com>
2023-01-11 03:06:25 +0530

    Update new.py

--- new.py
+++ new.py
@@ -21,7 +21,7 @@ if st.button('check'):
     img1 = np.asarray(img)
 #     st.write(img1)
     img1 = img1 / 255.0
-    st.write(img1)
+    st.write(type(img1))
     img1 = img1.resize((50,50,1))
     st.write(img1)
 #     img1 = np.array(img1)
